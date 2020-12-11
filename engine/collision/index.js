b63ad7643f942ba606aaff2e3a1c744fae1ffbfc
mrWeiss <giorgio2197@gmail.com>
2020-12-11 21:35:17 +0100

    fix spacing

--- engine/collision/index.js
+++ engine/collision/index.js
@@ -1,4 +1,2 @@
-import { CollisionGrid } from "./CollisionGrid";
-
-export {CollisionGrid, FloorGrid} from "./CollisionGrid.js"
-export {planesArray, planeFromTriangle, minMaxXZ, meshTransform} from "./utils.js"
+export {CollisionGrid, FloorGrid} from "./CollisionGrid.js";
+export {planesArray, planeFromTriangle, minMaxXZ, meshTransform} from "./utils.js";
