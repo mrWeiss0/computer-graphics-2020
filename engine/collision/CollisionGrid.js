43618c6ecab91815543162884db60796ab6e40b4
mrWeiss <giorgio2197@gmail.com>
2020-12-14 13:36:21 +0100

    add Floor class

--- engine/collision/CollisionGrid.js
+++ engine/collision/CollisionGrid.js
@@ -10,36 +10,27 @@ export class CollisionGrid {
 	}
 
 	addGeometry(frenchMesh) {
-		const vertices = frenchMesh.vertices;
-		const indices  = frenchMesh.indices;
-		const planes   = new Array(Math.floor(indices.length / 3));
-		const geometry = {mesh: frenchMesh, planes: planes};
-
+		const geometry = {
+			mesh   : frenchMesh,
+			planes : new Array(Math.floor(this.mesh.indices.length / 3))
+		};
 		for(let i = 0; i < planes.length; i++) {
-			const ax = vertices[indices[i * 3 + 0] * 3 + 0];
-			const ay = vertices[indices[i * 3 + 0] * 3 + 1];
-			const az = vertices[indices[i * 3 + 0] * 3 + 2];
-			const bx = vertices[indices[i * 3 + 1] * 3 + 0];
-			const by = vertices[indices[i * 3 + 1] * 3 + 1];
-			const bz = vertices[indices[i * 3 + 1] * 3 + 2];
-			const cx = vertices[indices[i * 3 + 2] * 3 + 0];
-			const cy = vertices[indices[i * 3 + 2] * 3 + 1];
-			const cz = vertices[indices[i * 3 + 2] * 3 + 2];
-
-			planes[i] = planeFromTriangle(ax, ay, az, bx, by, bz, cx, cy, cz);
-			this._addTriangle(ax, az, bx, bz, cx, cz, i, geometry);
+			const floor = new Floor(geometry, i);
+			const vert = floor.vertices;
+			geometry.planes[i] = planeFromTriangle(...vert);
+			const [ax, , az, bx, , bz, cx, , cz] = vert;
+			this._addTriangle(ax, az, bx, bz, cx, cz, floor);
 		}
-
 		this.geometries.push(geometry);
 	}
 
-	_addTriangle(ax, az, bx, bz, cx, cz, floorID, geometry) {
+	_addTriangle(ax, az, bx, bz, cx, cz, floor) {
 		const [ fromX, fromZ ] = this._findCell(Math.min(ax, bx, cx), Math.min(az, bz, cz));
 		const [   toX,   toZ ] = this._findCell(Math.max(ax, bx, cx), Math.max(az, bz, cz));
 
 		for (let i = fromX; i <= toX; i++)
 			for (let j = fromZ; j <= toZ; j++)
-				this._getCellList(i, j).push([floorID, geometry]);
+				this._getCellList(i, j).push(floor);
 	}
 
 	_findCell(x, z) {
@@ -52,6 +43,30 @@ export class CollisionGrid {
 		if(i < 0 || j < 0 || i >= this.nCells[0] || j >= this.nCells[1])
 			throw new Error("Cell out of bounds");
 		return this.grid[i + j * this.nCells[0]];
+	}
+}
+
+class Floor {
+	constructor(geometry, id) {
+		if(id >= geometry.planes.length)
+			throw new Error("Invalid floor ID");
+		this.geometry = geometry;
+		this.id       = id;
+	}
+
+	get vertices() {
+		const vertices = this.geometry.mesh.vertices;
+		const indices  = this.geometry.mesh.indices;
+		const vert     = new Array(9);
+		const floorID = this.id * 3;
+		for(let j = 0; j < 3; j++)
+			for(let i = 0; i < 3; i++)
+				vert[i + j * 3] = vertices[indices[floorID + j] * 3 + i];
+		return vert;
+	}
+
+	get plane() {
+		return this.geometry.planes[this.id];
 	}
 }
 
@@ -76,25 +91,12 @@ function planeFromTriangle(ax, ay, az, bx, by, bz, cx, cy, cz) {
 export class FloorGrid extends CollisionGrid {
 	findFloorHeight(x, y, z) {
 		const floorList = this._getCellList(...this._findCell(x, z));
-
-		if (!floorList) {
-			return undefined;
-		}
+		
 		let actualFloor;
 		//let maxHeight;
 		for (let floor of floorList) {
-			const geometry = this.geometries.get(floor[1]);
-			const plane = geometry.planes[floor[0]];
-			const vertices = geometry.mesh.vertices;
-			const indices = geometry.mesh.indices;
-			const i = floor[0] * 3;
-
-			const ax = vertices[indices[i] * 3];
-			const az = vertices[indices[i] * 3 + 2];
-			const bx = vertices[indices[i + 1] * 3];
-			const bz = vertices[indices[i + 1] * 3 + 2];
-			const cx = vertices[indices[i + 2] * 3];
-			const cz = vertices[indices[i + 2] * 3 + 2];
+			const [ax, , az, bx, , bz, cx, , cz] = floor.vertices;
+			const plane = floor.plane;
 
 			// Check that the point is within the triangle bounds.
 			if ((az - z) * (bx - ax) - (ax - x) * (bz - az) < 0) {
