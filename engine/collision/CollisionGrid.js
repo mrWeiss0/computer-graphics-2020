63bd9dde5524cc2bb1221e135ba7223c772214da
mrWeiss <giorgio2197@gmail.com>
2020-12-14 15:52:56 +0100

    remove CollisionGrid subclass

--- engine/collision/CollisionGrid.js
+++ engine/collision/CollisionGrid.js
@@ -1,5 +1,5 @@
-import {utils} from "./index.js";
-const Vec3 = utils.matrix.Vec3;
+
+const FLOOR_HITBOX = 78;
 
 export class CollisionGrid {
 	constructor(minX, maxX, minZ, maxZ, cellLenX, cellLenZ) {
@@ -21,72 +21,13 @@ export class CollisionGrid {
 			const floor = new Floor(geometry, i);
 			const vert = floor.vertices;
 			geometry.planes[i] = _planeFromTriangle(vert);
+			if(floor.normal.y <= 0)
+				continue;
 			this._addTriangle(vert, floor);
 		}
 		this.geometries.push(geometry);
 	}
 
-	_addTriangle([a, b, c], floor) {
-		const [ fromX, fromZ ] = this._findCell(Math.min(a.x, b.x, c.x), Math.min(a.z, b.z, c.z));
-		const [   toX,   toZ ] = this._findCell(Math.max(a.x, b.x, c.x), Math.max(a.z, b.z, c.z));
-
-		for (let i = fromX; i <= toX; i++)
-			for (let j = fromZ; j <= toZ; j++)
-				this._getCellList(i, j).push(floor);
-	}
-
-	_findCell(x, z) {
-		const i = Math.floor((x + this.offset[0]) / this.cellLen[0]);
-		const j = Math.floor((z + this.offset[1]) / this.cellLen[1]);
-		return [i, j];
-	}
-
-	_getCellList(j, i) {
-		return this.grid[i + j * this.nCells[0]];
-	}
-
-	_outOfBounds(i, j) {
-		return i < 0 || j < 0 || i >= this.nCells[0] || j >= this.nCells[1];
-	}
-}
-
-class Floor {
-	constructor(geometry, id) {
-		if(id >= geometry.planes.length)
-			throw new Error("Invalid floor ID");
-		this.geometry = geometry;
-		this.id       = id;
-	}
-
-	get vertices() {
-		const vertices = this.geometry.mesh.vertices;
-		const indices  = this.geometry.mesh.indices;
-		const vert     = Array.from({length : 3}, () => new Vec3(0));
-		const floorID = this.id * 3;
-		for(let j = 0; j < 3; j++)
-			for(let i = 0; i < 3; i++)
-				vert[j * 3].set(i, vertices[indices[floorID + j] * 3 + i]);
-		return vert;
-	}
-
-	get normal() {
-		return this.geometry.planes[this.id][0];
-	}
-
-	get offset() {
-		return this.geometry.planes[this.id][1];
-	}
-}
-
-function _planeFromTriangle([a, b, c]) {
-	const n = b.sub(a).cross(c.sub(a));
-	const d = -n.mul(a);
-	return [n, d];
-}
-
-const FLOOR_HITBOX = 78;
-
-export class FloorGrid extends CollisionGrid {
 	findFloorHeight(x, y, z) {
 		const [i, j] = this._findCell(x, z);
 		const found = {floor: null, height: -Infinity};
@@ -116,4 +57,33 @@ export class FloorGrid extends CollisionGrid {
 		}
 		return found;
 	}
+
+	_addTriangle([a, b, c], floor) {
+		const [ fromX, fromZ ] = this._findCell(Math.min(a.x, b.x, c.x), Math.min(a.z, b.z, c.z));
+		const [   toX,   toZ ] = this._findCell(Math.max(a.x, b.x, c.x), Math.max(a.z, b.z, c.z));
+
+		for (let i = fromX; i <= toX; i++)
+			for (let j = fromZ; j <= toZ; j++)
+				this._getCellList(i, j).push(floor);
+	}
+
+	_findCell(x, z) {
+		const i = Math.floor((x + this.offset[0]) / this.cellLen[0]);
+		const j = Math.floor((z + this.offset[1]) / this.cellLen[1]);
+		return [i, j];
+	}
+
+	_getCellList(j, i) {
+		return this.grid[i + j * this.nCells[0]];
+	}
+
+	_outOfBounds(i, j) {
+		return i < 0 || j < 0 || i >= this.nCells[0] || j >= this.nCells[1];
+	}
+}
+
+function _planeFromTriangle([a, b, c]) {
+	const n = b.sub(a).cross(c.sub(a));
+	const d = -n.mul(a);
+	return [n, d];
 }
--- engine/collision/index.js
+++ engine/collision/index.js
@@ -1,3 +1,4 @@
 export {utils} from "../index.js";
 export {CollisionGrid, FloorGrid} from "./CollisionGrid.js";
+export {Floor} from "./Floor.js";
 export {minMaxXZ, meshTransform} from "./utils.js";
